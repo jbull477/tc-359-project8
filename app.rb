0339534f0efa5bf6fe88892cff0da73008e8c140
jbull477 <jbull477@gmail.com>
2015-01-26 16:26:22 -0500

    user is directed to an html file now

--- app.rb
+++ app.rb
@@ -3,6 +3,6 @@ require 'sinatra'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-    "Welcome to the webapp of DOOM, where the copyright is always up to date! Copyright 2014-#{ Time.now.year } Jason Bull."
+    send_file 'views/index.html'
   end
 end
